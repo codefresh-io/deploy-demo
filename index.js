9708be7bb6c507620487b31d7a2048fdb891d043
Oleg verhovsky <verchol@gmail.com>
2016-12-11 08:15:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const request           = require('superagent');
+const measureTime       = require('measure-time');
+const html              = require('html');
+const colors            = require('colors');
+
+const getElapsed = measureTime();
+
+request
+    .post('g.codefresh.io')
+    .send({ name: 'Manny', species: 'cat' })
+    .set('X-API-Key', 'foobar')
+    .set('Accept', 'application/json')
+    .end(function(err, res){
+        // Calling the end function will send the request
+        const elapsed = JSON.stringify(getElapsed());;
+
+        console.log(`${html.prettyPrint(JSON.stringify(res))}`);
+
+        console.log(elapsed.green);
+    });
+
+
