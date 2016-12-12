324a254d67562ccd89532b3ed1f53f4b3bcfc09c
Oleg verhovsky <verchol@gmail.com>
2016-12-12 22:41:44 +0200

    fix

---
+++ build-info/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+const Table = require('cli-table');
+const colors = require('colors');
+const _      = require('lodash');
+
+var branch = process.env.CF_BRANCH || 'master';
+var sha = process.env.CF_REVISION|| 'empty';
+var shaMessage = process.env.CF_COMMIT_MESSAGE|| 'empty';
+
+
+
+var t = new Table({
+    head: ['Parameter', 'Value']
+   // , colWidths: [100, 200]
+});
+t.push({"CF_BRANCH" : branch.green});
+t.push({"sha" : sha.green});
+t.push({"shaMessage" : shaMessage.green});
+
+
+
+console.log(t.toString());
